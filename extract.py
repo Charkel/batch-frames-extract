1a47e9d5a1b5a2836d5c77f0226b1cfa6f9b3d0e
Charkel <charkel@gmail.com>
2024-05-04 21:17:03 +0200

    Update extract.py
    Added support for additional video file formats: MPEG, MPG, M4V, WebM, TS, VOB, 3GP, OGV, RM, ASF, DivX, Xvid, RMVB.

--- extract.py
+++ extract.py
@@ -61,7 +61,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Get a list of all video files in the input directory
-video_extensions = ["*.mp4", "*.avi", "*.mkv", "*.mov", "*.flv", "*.wmv"]
+video_extensions = ["*.mp4", "*.avi", "*.mkv", "*.mov", "*.flv", "*.wmv", "*.mpeg", "*.mpg", "*.m4v", "*.webm", "*.ts", "*.vob", "*.3gp", "*.ogv", "*.rm", "*.asf", "*.divx", "*.xvid", "*.rmvb"]
 video_paths = [glob.glob(os.path.join(input_dir, ext)) for ext in video_extensions]
 video_paths = [item for sublist in video_paths for item in sublist]
 
@@ -71,4 +71,3 @@ if not video_paths:
 
 for video_path in video_paths:
     extract_frames(video_path, output_dir)
-
